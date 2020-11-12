668e89e47808c679de7f0552757c42134ce70711
Marcus Wolschon <marcus.wolschon@googlemail.com>
2020-11-12 20:05:25 +0100

    initial code

--- shared/build.gradle.kts
+++ shared/build.gradle.kts
@@ -48,7 +48,7 @@ kotlin {
 }
 android {
     compileSdkVersion(29)
-    sourceSets["main"].manifest.srcFile("src\androidMain\AndroidManifest.xml")
+    sourceSets["main"].manifest.srcFile("src\\androidMain\\AndroidManifest.xml")
     defaultConfig {
         minSdkVersion(24)
         targetSdkVersion(29)
