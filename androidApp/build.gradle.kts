003b8c9db2fcda14bd6d3593724190a9a4b17ea6
Administrator <admin@example.com>
2020-11-13 12:14:26 +0000

    gradle

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -17,18 +17,8 @@ dependencies {
     implementation("com.google.android.material:material:1.2.1")
     implementation("androidx.appcompat:appcompat:1.2.0")
     implementation("androidx.constraintlayout:constraintlayout:2.0.4")
-}
-kotlin {
-    experimental {
-        coroutines('enable')
-//        unsignedTypes('enable')
-    }
-
-    dependencies {
-        implementation "org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7"
-        implementation "org.jetbrains.kotlinx:kotlinx-coroutines-android:1.2.1"
-    }
-
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.3.7")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.2.1")
 }
 android {
     compileSdkVersion(29)
