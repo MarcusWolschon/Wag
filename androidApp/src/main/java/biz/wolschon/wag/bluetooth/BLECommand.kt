8198f161550e0d22dd2fe68d255f3e7527d3b053
Administrator <admin@example.com>
2020-11-24 06:55:19 +0000

    isCommandCompatible

--- androidApp/src/main/java/biz/wolschon/wag/bluetooth/BLECommand.kt
+++ androidApp/src/main/java/biz/wolschon/wag/bluetooth/BLECommand.kt
@@ -7,6 +7,8 @@ import android.bluetooth.BluetoothGattCallback
 abstract class BLECommand : BluetoothGattCallback() {
 
     open val expectingResult: Boolean = false
+    open val isTailCommand: Boolean = true
+    open val isEarCommand: Boolean = true
 
     /**
      * @return false if unsuccessful and no callback is to be expected
