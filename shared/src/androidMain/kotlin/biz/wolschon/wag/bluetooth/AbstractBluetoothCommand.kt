937159b0088c7b8b8fab7bc59edeff199313634d
Administrator <admin@example.com>
2020-11-26 14:36:11 +0000

    #11 shared code

---
+++ shared/src/androidMain/kotlin/biz/wolschon/wag/bluetooth/AbstractBluetoothCommand.kt
@@ -0,0 +1,40 @@
+package biz.wolschon.wag.bluetooth.commands
+
+import androidx.lifecycle.MutableLiveData
+import android.bluetooth.BluetoothGatt
+import android.bluetooth.BluetoothGattCharacteristic
+import android.bluetooth.BluetoothGattDescriptor
+import android.util.Log
+import biz.wolschon.wag.bluetooth.BLECommand
+import biz.wolschon.wag.bluetooth.BLECommandQueue
+import biz.wolschon.wag.R
+import biz.wolschon.wag.bluetooth.DeviceConnection
+
+abstract class AbstractBluetoothCommand() : BLECommand() {
+
+    fun sendCommand(cmd: String): Boolean {
+        val characteristic = deviceConnection.controlOut
+            .apply {
+                value = cmd.toByteArray()
+            }
+        val result =  deviceConnection.bluetoothGatt.writeCharacteristic(characteristic)
+        expectingResult = result
+        return result
+    }
+
+    abstract fun onResultReceived(result: String)
+
+    override fun onCharacteristicChanged(
+        gatt: BluetoothGatt?,
+        characteristic: BluetoothGattCharacteristic?
+    ) {
+        val result = characteristic?.getStringValue(0)
+        Log.d(TAG, "onCharacteristicChanged '$result'")
+        onResultReceived(result)
+        super.onCharacteristicChanged(gatt, characteristic)
+    }
+
+    companion object {
+        private const val TAG = "AbstractBluetoothCommand"
+    }
+}
